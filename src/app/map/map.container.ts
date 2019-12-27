1cc29f8473ea72215658588acaeed7985e5ae308
maeda <takanori@stroly.jp>
2019-12-28 00:20:58 +0900

    mapboxglで地図を表示するまで実装

--- src/app/map/map.container.ts
+++ src/app/map/map.container.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 
+import * as mapboxgl from 'mapbox-gl';
+
 @Component({
   selector: 'app-map',
   template: `
@@ -15,5 +17,18 @@ export class MapContainerComponent implements OnInit {
   ngOnInit() {
     this.el = this.elementRef.nativeElement;
     const mapElem = this.el.querySelector('.map') as HTMLElement;
+
+    (mapboxgl as any).accessToken =
+      'pk.eyJ1IjoibWFlY2hhYmluIiwiYSI6ImNrNGU0eHYxMzAya3YzZm1odWRyYjAycmsifQ.dL1yZ_6587JwS6uYjwPkGg';
+
+    const map = new mapboxgl.Map({
+      container: mapElem,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [139.74267643565133, 35.69432984468491],
+      zoom: 14,
+      pitch: 45,
+      bearing: -17.6,
+      antialias: true,
+    });
   }
 }
